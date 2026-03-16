2c506c2768c2a24cf334ca45d7a9c10280b89112
CamilleDU3 <camille3du@gmail.com>
2026-03-16 14:43:03 +0100

    feat(calculator-page): add Button component

---
+++ frontend/components/Button.tsx
@@ -0,0 +1,29 @@
+'use client';
+import { cva, type VariantProps } from 'class-variance-authority';
+
+const buttonStyles = cva('', {
+    variants: {
+        variant: {
+            secondary:
+                'bg-background text-foreground border border-gray-300 rounded-lg mx-4 px-6 py-3 hover:bg-hover transition-colors duration-300',
+        },
+    },
+    defaultVariants: {
+        variant: 'secondary',
+    },
+});
+type ButtonStylesTypes = VariantProps<typeof buttonStyles>;
+type ButtonVariant = ButtonStylesTypes['variant'];
+
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+    variant?: ButtonVariant;
+};
+
+export default function Button({ variant, children, ...props }: ButtonProps) {
+    const buttonClassName = buttonStyles({ variant });
+    return (
+        <button className={buttonClassName} {...props}>
+            {children}
+        </button>
+    );
+}
