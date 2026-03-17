f58b1f4595ba1a64973067f39728ce6860b658b2
CamilleDU3 <camille3du@gmail.com>
2026-03-17 10:40:35 +0100

    fix(component): correct Input name to InputNumber

--- frontend/components/InputNumber.tsx
+++ frontend/components/InputNumber.tsx
@@ -5,7 +5,7 @@ type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
 };
 //TODO: add a onBlur event, remove 0 firt digit ex: "0010000" -> "10000"
 //TODO: change to type="text" for thousand-value separation (to include if implemented : strip space)
-export default function Input({
+export default function InputNumber({
     id,
     labelText,
     name,
